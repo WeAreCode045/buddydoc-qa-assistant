7e008b8eb803216dde625f3c9cbfd0df5a3e97cf
Code045 <maurice@code045.nl>
2025-01-15 06:39:35 +0100

    cors

--- functions.php
+++ functions.php
@@ -12,30 +12,7 @@ function add_cors_headers() {
 }
 add_action('init', 'add_cors_headers');
 
-// Add PDF proxy endpoint
-function register_pdf_proxy_endpoint() {
-    register_rest_route('pdf-proxy/v1', '/proxy-pdf', array(
-        'methods' => 'GET',
-        'callback' => 'proxy_pdf_file',
-        'permission_callback' => '__return_true'
-    ));
-}
-add_action('rest_api_init', 'register_pdf_proxy_endpoint');
 
-function proxy_pdf_file($request) {
-    $url = $request->get_param('url');
-    if (!$url) {
-        return new WP_Error('no_url', 'No URL provided', array('status' => 400));
-    }
-
-    // Add error logging
-    error_log('Proxying PDF from URL: ' . $url);
-
-    $response = wp_remote_get($url);
-    if (is_wp_error($response)) {
-        error_log('Error fetching PDF: ' . $response->get_error_message());
-        return $response;
-    }
 
     $body = wp_remote_retrieve_body($response);
     $content_type = wp_remote_retrieve_header($response, 'content-type');
@@ -49,6 +26,8 @@ function proxy_pdf_file($request) {
     header('Content-Type: ' . $content_type);
     header('Content-Length: ' . strlen($body));
     header('Content-Disposition: inline; filename="document.pdf"');
+    header("Access-Control-Allow-Origin: *");
+
     
     echo $body;
     exit;
