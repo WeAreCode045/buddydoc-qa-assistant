5c7ab952f9c76e30b668c134598c7518f0b6ae0b
Code045 <maurice@code045.nl>
2025-01-15 06:47:26 +0100

    cors

--- functions.php
+++ functions.php
@@ -5,6 +5,9 @@ function add_cors_headers() {
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        status_header(200);
+        exit();
     }
 }
 add_action('init', 'add_cors_headers');
@@ -23,8 +26,6 @@ add_action('init', 'add_cors_headers');
     header('Content-Type: ' . $content_type);
     header('Content-Length: ' . strlen($body));
     header('Content-Disposition: inline; filename="document.pdf"');
-    header("Access-Control-Allow-Origin: *");
-
     
     echo $body;
     exit;
